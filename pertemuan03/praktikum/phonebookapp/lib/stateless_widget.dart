cb8f169f15c280388072883931fcc16d007ff31b
AdeCandra12 <ade.can321@gmail.com>
2023-10-16 16:42:49 +0700

    add

---
+++ pertemuan03/praktikum/phonebookapp/lib/stateless_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 28.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String teks;
+  const BiggerText({Key? key, required this.teks}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          widget.teks,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        ElevatedButton(
+          child: Text(_textSize == 16.0 ? "Perbesar" : "Perkecil"),
+          onPressed: () {
+            setState(() {
+              _textSize = _textSize == 16.0 ? 25.0 : 16.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
